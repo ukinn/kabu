b3cccb3bf7e3d280f30f8567684023a3647f50fa
Hokuto Handa <hokuto_1121@icloud.com>
2020-03-30 10:48:46 +0900

    インプットを表代わりに使うことはできない

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { savePrice, getPrice } from '../actions';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 // import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
 
 // const kabukas = [
 //   {sun: '100'},
@@ -21,50 +22,65 @@ import { Field, reduxForm } from 'redux-form';
 //   {sat_pm: ''},
 // ]
 //
-// const kabukas2 = {
-//   sun: '100',
-//   mon_am: '',
-//   mon_pm: '',
-//   tue_am: '',
-//   tue_pm: '',
-//   wed_am: '',
-//   wed_pm: '',
-//   thu_am: '',
-//   thu_pm: '',
-//   fri_am: '',
-//   fri_pm: '',
-//   sat_am: '',
-//   sat_pm: '',
-// }
-//
-// console.log(kabukas);
-// // console.log(kabukas2.sun);
-// kabukas[0] = {sun: '100'};
-// console.log(kabukas[0]);
-// console.log(kabukas);
-//
+const dates = [
+  {en: '日曜日',ja: '日'},
+  {en: 'mon_am', ja: '月ＰＭ'},
+  {en: 'mon_pm', ja: '月ＡＭ'},
+  {en: 'tue_am', ja: '火ＰＭ'},
+  {en: 'tue_pm', ja: '火ＡＭ'},
+  {en: 'wed_am', ja: '水ＰＭ'},
+  {en: 'wed_pm', ja: '水ＡＭ'},
+  {en: 'thu_am', ja: '木ＰＭ'},
+  {en: 'thu_pm', ja: '木ＡＭ'},
+  {en: 'fri_am', ja: '金ＰＭ'},
+  {en: 'fri_pm', ja: '金ＡＭ'},
+  {en: 'sat_am', ja: '土ＰＭ'},
+  {en: 'sat_pm', ja: '土ＡＭ'},
+];
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.submit = this.submit.bind(this);
+    this.renderFields = this.renderFields.bind(this);
   }
   submit(values){
     const { savePrice } = this.props;
     savePrice(values);
     // getPrice();
   }
+  componentDidMount(){
+    const { getPrice } = this.props;
+    getPrice();
+  }
+  renderFields(){
+    const fields = dates.map((date,indexs)=>{
+      const renderComponents = (field)=> {
+        //redux-form の設定を取得
+        const { input } = field;
+        return(
+          <TextField fullWidth label={date.ja} {...input} />
+        )};
+      return (
+      <Field key={date.en} name={date.en} label={date.ja} component={renderComponents} type="number" />
+    )}
+  );
+    return fields;
+  }
+
   render(){
-    const { handleSubmit } = this.props;
+    const { handleSubmit, prices } = this.props;
     return(
       <div className="wrapper">
       <h1>
         あつ森カブ価記録帳
       </h1>
       <form onSubmit={handleSubmit(this.submit)}>
-        <Field name="sun" component="input" type="text" />
+        {this.renderFields()}
         <button type="submit">Submit</button>
       </form>
+      <p>money:{prices.sun}</p>
       </div>
     );
   };
@@ -72,7 +88,10 @@ class App extends Component {
 
 const mapStateToProps = (state) => {
   return(
-    {prices: state.prices}
+    {
+      prices: state.prices,
+      initialValues: state.prices,
+    }
   )
 };
 
--- src/reducers/prices.js
+++ src/reducers/prices.js
@@ -2,7 +2,7 @@
 import { GET, SAVE } from '../actions';
 
 let initialState = {
-  sun: '100',
+  日曜日: '100',
   mon_am: '',
   mon_pm: '',
   tue_am: '',
@@ -16,19 +16,37 @@ let initialState = {
   sat_am: '',
   sat_pm: '',
 };
+// let initialState = {
+//   sun: '100',
+//   mon_am: '1',
+//   mon_pm: '11',
+//   tue_am: '111',
+//   tue_pm: '111',
+//   wed_am: '13',
+//   wed_pm: '534',
+//   thu_am: '35',
+//   thu_pm: '53',
+//   fri_am: '346',
+//   fri_pm: '364',
+//   sat_am: '7',
+//   sat_pm: '45',
+// };
 
 export default (prices = initialState, action) => {
   switch(action.type) {
     case GET:
       console.log('getきた')
-      return prices;
-    case SAVE:
-      console.log(action);
-      prices = action.values;
+      prices = JSON.parse(localStorage.getItem('kabuka')) || prices;
       console.log(prices);
       return prices;
+    case SAVE:
+      // console.log(action);
+      prices = action.values;
+      console.log(prices);
+      localStorage.setItem('kabuka', JSON.stringify(prices));
+      return prices;
     default:
-    console.log('何にもきてない')
+    // console.log('何にもきてない');
       return prices;
   }
 }
