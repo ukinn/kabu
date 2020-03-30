a8cfcf26b9f4feb4c5ce8982ac92739fdc7f8d66
Hokuto Handa <hokuto_1121@icloud.com>
2020-03-30 16:31:28 +0900

    完成（１回目）

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,61 @@
 import React, { Component } from 'react';
 
-import { savePrice, getPrice } from '../actions';
+import { savePrice, getPrice, resetPrice } from '../actions';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
-// import styled from 'styled-components';
+
+import styled from 'styled-components';
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-// const kabukas = [
-//   {sun: '100'},
-//   {mon_am: ''},
-//   {mon_pm: ''},
-//   {tue_am: ''},
-//   {tue_pm: ''},
-//   {wed_am: ''},
-//   {wed_pm: ''},
-//   {thu_am: ''},
-//   {thu_pm: ''},
-//   {fri_am: ''},
-//   {fri_pm: ''},
-//   {sat_am: ''},
-//   {sat_pm: ''},
-// ]
-//
+import { styled as uiStyled } from '@material-ui/core/styles';
+
+const MyPaper = uiStyled(Paper)({
+  margin: '10px',
+  padding: '20px'
+})
+const MyTableContainer = uiStyled(TableContainer)({
+  margin: '10px',
+  padding:'10px',
+});
+const ResetButton = uiStyled(Button)({
+  margin:'0 auto',
+  display: 'block',
+});
+
+const MyHeader = styled.h1`
+  text-align: center;
+  color: white;
+`;
+
+
 const dates = [
-  {en: 'sun', ja: '日曜'},
-  {en: 'mon_am', ja: '月曜ＰＭ'},
-  {en: 'mon_pm', ja: '月曜ＡＭ'},
-  {en: 'tue_am', ja: '火曜ＰＭ'},
-  {en: 'tue_pm', ja: '火曜ＡＭ'},
-  {en: 'wed_am', ja: '水曜ＰＭ'},
-  {en: 'wed_pm', ja: '水曜ＡＭ'},
-  {en: 'thu_am', ja: '木曜ＰＭ'},
-  {en: 'thu_pm', ja: '木曜ＡＭ'},
-  {en: 'fri_am', ja: '金曜ＰＭ'},
-  {en: 'fri_pm', ja: '金曜ＡＭ'},
-  {en: 'sat_am', ja: '土曜ＰＭ'},
-  {en: 'sat_pm', ja: '土曜ＡＭ'},
+  {en: 'sun', ja: '日曜(買値)'},
+  {en: 'mon_am', ja: '月曜ＡＭ'},
+  {en: 'mon_pm', ja: '月曜ＰＭ'},
+  {en: 'tue_am', ja: '火曜ＡＭ'},
+  {en: 'tue_pm', ja: '火曜ＰＭ'},
+  {en: 'wed_am', ja: '水曜ＡＭ'},
+  {en: 'wed_pm', ja: '水曜ＰＭ'},
+  {en: 'thu_am', ja: '木曜ＡＭ'},
+  {en: 'thu_pm', ja: '木曜ＰＭ'},
+  {en: 'fri_am', ja: '金曜ＡＭ'},
+  {en: 'fri_pm', ja: '金曜ＰＭ'},
+  {en: 'sat_am', ja: '土曜ＡＭ'},
+  {en: 'sat_pm', ja: '土曜ＰＭ'},
 ];
 
 
@@ -48,8 +65,10 @@ class App extends Component {
     this.submit = this.submit.bind(this);
     this.renderDateSelect = this.renderDateSelect.bind(this);
     this.renderTextField = this.renderTextField.bind(this);
+    this.renderTable = this.renderTable.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
-
+  //localStorageのデータを取得
   componentDidMount(){
     const { getPrice } = this.props;
     getPrice();
@@ -69,7 +88,7 @@ class App extends Component {
       const { input } = field;
       return (
         <div>
-          <InputLabel htmlFor="date-select">曜日</InputLabel>
+          <InputLabel htmlFor="date-select">日時</InputLabel>
           <Select {...input} native inputProps={{
               id: 'date-select',
             }}>
@@ -79,43 +98,84 @@ class App extends Component {
         </div>
       );
     };
-
     return(
       <div>
-        <Field name="date" component={renderComponent}>
-        </Field>
+        <Field name="date" component={renderComponent}></Field>
       </div>
     );
   }
-
   renderTextField(){
     const renderComponent = (field)=> {
       const { input } = field;
-      return (<TextField label="カブ価" {...input} />)
+      return (<TextField label="カブ価(ベル)" {...input} />)
     };
     return (
-      <div>
-        <Field name="price" component={renderComponent} type="number" />
-      </div>
+        <div>
+          <Field name="price" component={renderComponent} type="number" />
+        </div>
     )
+  }
+  renderTable(){
+    const { prices } = this.props;
+    const renderTableCells = ()=> {
+      const tableCells = dates.map((date,index)=>{
+        return(
+          <TableRow key={date.en}>
+            <TableCell>{date.ja}</TableCell>
+            <TableCell>{prices[date.en]}</TableCell>
+          </TableRow>
+        )
+      })
+      return tableCells;
+    }
+    return(
+      <MyTableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>日時</TableCell>
+              <TableCell>カブ価(ベル)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {renderTableCells()}
+          </TableBody>
+        </Table>
+      </MyTableContainer>
+    );
+  }
+  handleReset(){
+    const { resetPrice } = this.props;
+    resetPrice();
   }
 
   render(){
-    const { handleSubmit, prices } = this.props;
+    const { handleSubmit } = this.props;
     return(
-      <div className="wrapper">
-      <h1>
-        あつ森カブ価記録帳
-      </h1>
-      <form onSubmit={handleSubmit(this.submit)}>
-        <FormControl>
-          {this.renderDateSelect()}
-        </FormControl>
-        {this.renderTextField()}
-        <button type="submit">Submit</button>
-      </form>
-      <p>money:{prices.sun}</p>
-      </div>
+      <Container maxWidth="xs">
+        <MyHeader>
+          あつ森カブ価記録帳
+        </MyHeader>
+        <form onSubmit={handleSubmit(this.submit)}>
+          <MyPaper>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <FormControl>
+                  {this.renderDateSelect()}
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                {this.renderTextField()}
+              </Grid>
+              <Grid item xs={12}>
+                <div><Button variant="contained" color="primary" fullWidth type="submit">記録する</Button></div>
+              </Grid>
+            </Grid>
+          </MyPaper>
+        </form>
+        {this.renderTable()}
+        <ResetButton onClick={this.handleReset} variant="contained" color="secondary">リセット</ResetButton>
+      </Container>
     );
   };
 }
@@ -127,6 +187,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
   getPrice: ()=> dispatch(getPrice),
   savePrice: (values)=> dispatch(savePrice(values)),
+  resetPrice: ()=> dispatch(resetPrice),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({ form:'KABUKA' })(App));
